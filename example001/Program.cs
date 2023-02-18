79bd5e230f6d1ed70bd184ac84c2d8bd04a94d18
Natali <piratika309@mail.ru>
2023-02-18 18:50:00 +0300

    Двумерный массив с вещественными числами

---
+++ example001/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+Console.WriteLine("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+double [,] array = new double [n,m];
+Random random = new Random();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = random.NextDouble()*100;
+        Console.Write($"{array[i,j]}\t");
+    }
+     Console.WriteLine();
+}
