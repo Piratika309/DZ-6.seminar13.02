131e150479030cf3dc27c15e1371d3ee44b10cac
Natali <piratika309@mail.ru>
2023-02-18 21:50:59 +0300

    массив с правилом m+n

---
+++ example002/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+Console.WriteLine("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int [,] array = new int [n,m];
+Random random = new Random();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = i+j;
+        Console.Write($"{array[i,j]}\t");
+    }
+     Console.WriteLine();
+}
